ab41955390200fdc426b642edc423d58a7d8560f
Macy Pruitt <macypruitt@gmail.com>
2019-08-30 16:40:25 -0500

    created express route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const feedback = require('./routes/feedback.router')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
@@ -10,7 +11,7 @@ app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
-
+app.use('/feedback', feedback)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
