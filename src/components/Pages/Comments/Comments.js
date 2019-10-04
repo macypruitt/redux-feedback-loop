538c7babbfbf5af4ccced514f908b33980ee604e
Macy Pruitt <macypruitt@gmail.com>
2019-10-04 16:02:04 -0500

    updated ReadME

--- src/components/Pages/Comments/Comments.js
+++ src/components/Pages/Comments/Comments.js
@@ -27,7 +27,7 @@ class Comments extends Component {
         return (
             <div>
                 <h3>Any comments you want to leave?</h3>
-                <input  className="comments-box" onChange={this.handleChange} type="text" name="rating"></input>
+                <textarea  className="comments-box" onChange={this.handleChange}></textarea>
                 <button onClick ={this.handleClickSubmit} type="submit">Submit</button> 
             </div>
         )
